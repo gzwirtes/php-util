cd6d756531672b0e5599fd5828e8d2362a69c4ae
Gustavo Zwirtes <gustavo.zts@gmail.com>
2023-07-18 12:33:42 -0300

    ajuste trim class TRecord personalizada

--- src/adianti/database/TRecord.php
+++ src/adianti/database/TRecord.php
@@ -3,21 +3,26 @@ namespace GZInfo\Adianti\Database;
 
 class TRecord extends \Adianti\Database\TRecord
 {
-    public function default_values(){
+    public function default_values()
+    {
 
     }
 
     public function onAfterLoad($object)
     {
-        foreach ($object as $key => $value) {
-            $object->$key = trim($value);
+        foreach ($object as $key => $value)
+        {
+            if(!is_null($value))
+                $object->$key = trim($value);
         }
     }
 
     public function onAfterLoadCollection($object)
     {
-        foreach ($object as $key => $value) {
-            $object->$key = trim($value);
+        foreach ($object as $key => $value)
+        {
+            if(!is_null($value))
+                $object->$key = trim($value);
         }
     }
 }
